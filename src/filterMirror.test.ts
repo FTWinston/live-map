63a035929f05f21f0d44459f1f8920bce368bb84
FTWinston <ftwinston@gmail.com>
2020-05-16 23:28:04 +0100

    Now allowing string remapping and mapping functions

--- src/filterMirror.test.ts
+++ src/filterMirror.test.ts
@@ -1,24 +1,29 @@
 import { filterMirror } from './filterMirror';
 
-interface Mirror {
+interface FlatSource {
     prop1: string;
-    prop3: boolean;
+    prop2: boolean;
+    prop3: number;
+    prop4?: string;
+    array?: string[];
 }
 
-interface FlatSource extends Mirror {
-    prop2: number;
+interface FlatMirror {
+    prop1: string;
+    prop2: boolean;
+    length?: number;
 }
 
 test('copies flat object', () => {
     const source: FlatSource = {
         prop1: 'hello',
-        prop2: 35,
-        prop3: false,
+        prop2: false,
+        prop3: 35,
     };
 
-    const [proxy, mirror] = filterMirror<FlatSource, Mirror>(source, {
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
         prop1: true,
-        prop3: true,
+        prop2: true,
     });
 
     expect(proxy).toHaveProperty('prop1');
@@ -30,25 +35,25 @@ test('copies flat object', () => {
 
     expect(mirror).toHaveProperty('prop1');
     expect(mirror.prop1).toEqual(source.prop1);
-    expect(mirror).not.toHaveProperty('prop2');
-    expect(mirror).toHaveProperty('prop3');
-    expect(mirror.prop3).toEqual(source.prop3);
+    expect(mirror).toHaveProperty('prop2');
+    expect(mirror.prop2).toEqual(source.prop2);
+    expect(mirror).not.toHaveProperty('prop3');
 });
 
 test('mirrors relevant property changes', () => {
     const source: FlatSource = {
         prop1: 'hello',
-        prop2: 35,
-        prop3: false,
+        prop2: false,
+        prop3: 35,
     };
 
-    const [proxy, mirror] = filterMirror<FlatSource, Mirror>(source, {
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
         prop1: true,
-        prop3: true,
+        prop2: true,
     });
 
     proxy.prop1 = 'bye';
-    proxy.prop2 = 27;
+    proxy.prop3 = 27;
 
     expect(proxy).toHaveProperty('prop1');
     expect(proxy.prop1).toEqual(source.prop1);
@@ -57,10 +62,105 @@ test('mirrors relevant property changes', () => {
     expect(proxy.prop2).toEqual(source.prop2);
     expect(proxy).toHaveProperty('prop3');
     expect(proxy.prop3).toEqual(source.prop3);
+    expect(proxy).toHaveProperty('prop3');
+    expect(proxy.prop3).toEqual(source.prop3);
 
     expect(mirror).toHaveProperty('prop1');
     expect(mirror.prop1).toEqual(source.prop1);
-    expect(mirror).not.toHaveProperty('prop2');
-    expect(mirror).toHaveProperty('prop3');
-    expect(mirror.prop3).toEqual(source.prop3);
+    expect(mirror).toHaveProperty('prop2');
+    expect(mirror.prop2).toEqual(source.prop2);
+    expect(mirror).not.toHaveProperty('prop3');
+});
+
+test('sets up string mapping', () => {
+    const source: FlatSource = {
+        prop1: 'hello',
+        prop2: false,
+        prop3: 35,
+        prop4: 'hi',
+    };
+
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
+        prop4: 'prop1',
+    });
+
+    expect(proxy).toHaveProperty('prop1');
+    expect(proxy.prop1).toEqual(source.prop1);
+    expect(proxy.prop1).toEqual('hello');
+    
+    expect(mirror).toHaveProperty('prop1');
+    expect(mirror.prop1).toEqual(proxy.prop4);
+    expect(mirror.prop1).toEqual('hi');
+});
+
+test('string maps property changes', () => {
+    const source: FlatSource = {
+        prop1: 'hello',
+        prop2: false,
+        prop3: 35,
+        prop4: 'hi',
+    };
+
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
+        prop4: 'prop1',
+    });
+
+    proxy.prop1 = 'howdy';
+    proxy.prop4 = 'bye';
+
+    expect(proxy).toHaveProperty('prop1');
+    expect(proxy.prop1).toEqual(source.prop1);
+    expect(proxy.prop1).toEqual('howdy');
+    
+    expect(mirror).toHaveProperty('prop1');
+    expect(mirror.prop1).toEqual(proxy.prop4);
+    expect(mirror.prop1).toEqual('bye');
+});
+
+test('sets up function mapping', () => {
+    const source: FlatSource = {
+        prop1: 'hello',
+        prop2: false,
+        prop3: 35,
+        prop4: 'hi',
+        array: ['blah', 'blabber'],
+    };
+
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
+        array: (dest, val) => dest.length = val.length,
+    });
+
+    expect(proxy).toHaveProperty('array');
+    expect(proxy.array).toHaveLength(2);
+    expect(proxy.array[0]).toEqual('blah');
+    expect(proxy.array[1]).toEqual('blabber');
+    
+    expect(mirror).toHaveProperty('length');
+    expect(mirror.length).toEqual(2);
+});
+
+test('function maps property changes', () => {
+    const source: FlatSource = {
+        prop1: 'hello',
+        prop2: false,
+        prop3: 35,
+        prop4: 'hi',
+        array: ['blah', 'blabber'],
+    };
+
+    const [proxy, mirror] = filterMirror<FlatSource, FlatMirror>(source, {
+        array: (dest, val) => dest.length = val.length,
+    });
+
+    // TODO: what if we mutate the array, though? That needs something more complicated...
+    proxy.array = ['hi', 'de', 'hey'];
+
+    expect(proxy).toHaveProperty('array');
+    expect(proxy.array).toHaveLength(3);
+    expect(proxy.array[0]).toEqual('hi');
+    expect(proxy.array[1]).toEqual('de');
+    expect(proxy.array[2]).toEqual('hey');
+    
+    expect(mirror).toHaveProperty('length');
+    expect(mirror.length).toEqual(3);
 });
--- src/filterMirror.ts
+++ src/filterMirror.ts
@@ -1,33 +1,66 @@
 type Filter<TSource, TMirror> = {
-    //[P in keyof TSource]?: true;
-    [P in Extract<keyof TSource, keyof TMirror>]?: true;
+    // Allow boolean and string remapping for keys present in both types.
+    [P in Extract<keyof TSource, keyof TMirror>]?:
+        boolean | keyof TMirror;
+} & {
+    // Allow string remapping, and mapping functions for keys present only in source type.
+    [P in Exclude<keyof TSource, keyof TMirror>]?:
+        keyof TMirror | ((dest: TMirror, value: any, source: TSource) => void);
 }
 
 export function filterMirror<TSource extends {}, TMirror extends {}>(source: TSource, filter: Filter<TSource, TMirror>): [TSource, TMirror] {
-    type CombinedKeys = keyof TSource & keyof TMirror;
-    
-    const keysToMirror = new Set<CombinedKeys>(Object.keys(filter) as [CombinedKeys]);
+    type Operation = (dest: TMirror, value: any, source: TSource) => void;
+
+    // TODO: need to store in here the destination properties to delete, too
+    const fieldOperations = new Map<keyof TSource, Operation>();
+
+    for (const key of Object.keys(filter)) {
+        let filterValue = filter[key as keyof Filter<TSource, TMirror>];
+        
+        if (filterValue === false) {
+            continue;
+        }
+
+        let operation: Operation;
+
+        if (filterValue === true) {
+            operation = (dest, val) => dest[key as keyof TMirror] = val;
+        }
+        else if (typeof filterValue === 'function') {
+            operation = filterValue as Operation;
+        }
+        else if (typeof filterValue === 'string' || typeof filterValue === 'number' || typeof filterValue === 'symbol') {
+            const destParam = filterValue as keyof TMirror;
+            operation = (dest, val) => dest[destParam] = val;
+        }
+        else {
+            throw new Error(`Filter value has unexpected type: ${filterValue}`);
+        }
+
+        fieldOperations.set(key as keyof TSource, operation);
+    }
 
     const mirror: TMirror = {} as unknown as TMirror;
-    for (const key of keysToMirror) {
-        mirror[key] = source[key] as any;
+    for (const [key, operation] of fieldOperations) {
+        operation(mirror, source[key], source)
     }
 
     const proxy = new Proxy(source, {
-        set: (target, param: CombinedKeys, val) => {
+        set: (target, param: keyof TSource, val) => {
             target[param] = val;
 
-            if (keysToMirror.has(param)) {
-                mirror[param] = val;
+            const operation = fieldOperations.get(param);
+            if (operation) {
+                operation(mirror, val, source);
             }
-
+            
             return true;
         },
-        deleteProperty: (target, param: CombinedKeys) => {
+        deleteProperty: (target, param: keyof TSource) => {
             delete target[param];
 
-            if (keysToMirror.has(param)) {
-                delete mirror[param];
+            if (fieldOperations.has(param)) {
+                //delete mirror[param];
             }
 
             return true;
